f667e96a9501ab3518892165d327457f984a4379
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-22 09:48:06 +0100

    Initial commit: start code

---
+++ android/app/src/main/kotlin/isetbz/rnu/tn/dsi3_navigate_named_routes/MainActivity.kt
@@ -0,0 +1,6 @@
+package isetbz.rnu.tn.dsi3_navigate_named_routes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/third_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MyThirdScreen extends StatelessWidget {
+  const MyThirdScreen({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: Colors.teal,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "Screen n°2",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                color: Colors.teal
+              ),
+            ),
+            const Padding(padding: EdgeInsets.only(bottom: 20)),
+            RaisedButton(
+              child: const Text("Previous Screen"),
+                color: Colors.teal,
+                textColor: Colors.white,
+                onPressed: () {
+                  Navigator.pop(context);
+                  //Navigator.of(context).pop(); // alternative syntax
+                }
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
